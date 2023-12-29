noir19/ugit

data: Dereference refs when reading and writing
Now we'll dereference symbolic refs not only when reading them but also when
writing them.

We'll implement a helper function called `_get_ref_internal` which will return the
path and the value of the last ref pointed by a symbolic ref. In simple words:

- When given a non-symbolic ref, `_get_ref_internal` will return the ref name and
  value.
- When given a symbolic ref, `_get_ref_internal` will dereference the ref
  recursively, and then return the name of the last (non-symbolic) ref that
  points to an OID, plus its value.

Now `update_ref` will use `_get_ref_internal` to know which ref it needs to update.

Additionaly, we'll use `_get_ref_internal` in `get_ref`.

%% METADATA
Id: deref-symbolic-refs-on-write

ugit/data.py
@@ -16,6 +16,7 @@ RefValue = namedtuple ('RefValue', ['symbolic', 'value'])
 
 def update_ref (ref, value):
     assert not value.symbolic
+    ref = _get_ref_internal (ref)[0]
     ref_path = f'{GIT_DIR}/{ref}'
     os.makedirs (os.path.dirname (ref_path), exist_ok=True)
     with open (ref_path, 'w') as f:
@@ -23,16 +24,22 @@ def update_ref (ref, value):
 
 
 def get_ref (ref):
+    return _get_ref_internal (ref)[1]
+
+
+def _get_ref_internal (ref):
     ref_path = f'{GIT_DIR}/{ref}'
     value = None
     if os.path.isfile (ref_path):
         with open (ref_path) as f:
             value = f.read ().strip ()
 
-    if value and value.startswith ('ref:'):
-        return get_ref (value.split (':', 1)[1].strip ())
+    symbolic = bool (value) and value.startswith ('ref:')
+    if symbolic:
+        value = value.split (':', 1)[1].strip ()
+        return _get_ref_internal (value)
 
-    return RefValue (symbolic=False, value=value)
+    return ref, RefValue (symbolic=symbolic, value=value)
 
 
 def iter_refs ():

VoidcloudGuru/Project_space

ugit/data.py
@@ -16,6 +16,7 @@ RefValue = namedtuple ('RefValue', ['symbolic', 'value'])
 
 def update_ref (ref, value):
     assert not value.symbolic
+    ref = _get_ref_internal (ref)[0]
     ref_path = f'{GIT_DIR}/{ref}'
     os.makedirs (os.path.dirname (ref_path), exist_ok=True)
     with open (ref_path, 'w') as f:
@@ -23,16 +24,22 @@ def update_ref (ref, value):
 
 
 def get_ref (ref):
+    return _get_ref_internal (ref)[1]
+
+
+def _get_ref_internal (ref):
     ref_path = f'{GIT_DIR}/{ref}'
     value = None
     if os.path.isfile (ref_path):
         with open (ref_path) as f:
             value = f.read ().strip ()
 
-    if value and value.startswith ('ref:'):
-        return get_ref (value.split (':', 1)[1].strip ())
+    symbolic = bool (value) and value.startswith ('ref:')
+    if symbolic:
+        value = value.split (':', 1)[1].strip ()
+        return _get_ref_internal (value)
 
-    return RefValue (symbolic=False, value=value)
+    return ref, RefValue (symbolic=False, value=value)
 
 
 def iter_refs ():